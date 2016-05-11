9a88849da8a173323f72ab8953299e92a2784324
Jason Greenstreet <jason.greenstreet@outlook.com>
2016-05-11 11:07:35 -0500

    Add /todos and /todo/:id

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: 'Meet someone for lunch',
+  completed: false
+},{
+  id: 2,
+  description: 'go to store',
+  completed: false
+},{
+  id: 3,
+  description: 'go to graduation',
+  completed: true
+}];
 
 app.get('/',function(req,res){
   res.send('TODO API root');
+});
+
+app.get('/todos',function(req,res){
+  res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res){
+  var todoId = parseInt(req.params.id);
+  var matchedTodo;
+
+  todos.forEach(function (todo) {
+    if (todoId === todo.id){
+      matchedTodo = todo;
+    }
+  });
+  if (matchedTodo){
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.listen(PORT,function(){
